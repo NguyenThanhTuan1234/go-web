e44f1e16104ead319e79dbeb0d3a864e2711d118
TUANKUN <Tuankun@TuanKunnoMacBook-Pro.local>
2020-07-23 11:18:08 +0900

    微修正

--- handler/index.go
+++ handler/index.go
@@ -1,12 +1,10 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	u := GetUser(w, r)
-	fmt.Println(u)
 	tpl.ExecuteTemplate(w, "index.html", u)
 }
